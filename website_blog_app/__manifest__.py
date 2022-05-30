507d4e28f411d261c1939db81d795be142fb5e95
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-05-30 16:05:40 +0200

    Update __manifest__.py

--- website_blog_app/__manifest__.py
+++ website_blog_app/__manifest__.py
@@ -1,16 +1,40 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'Website Blog App',
-    'category': 'Website/Website',
-    'sequence': 200,
-    'website': 'https://www.vertlab.se',
+    'name': 'Website Blog: Website Blog App',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Website Blog App',
+    'category': 'Website',
+    'description': '',
+    #'sequence': '200',
     'author': 'Vertel AB',
-    'version': '1.0',
-    'description': "",
-    'depends': ['website_blog'],
+    'website': 'https://vertel.se/apps/odoo-website-blog/website_blog_app',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website-blog',
+    # 'depends': ['website_blog'],
     'data': [
         'views/snippets/snippets.xml',
         'views/website_blog_view.xml',
@@ -23,5 +47,4 @@
     ],
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
 }
